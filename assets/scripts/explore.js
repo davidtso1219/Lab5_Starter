51dec076672113f3b849b47995a56a0a8313214f
kainoaw <kwoessner@ucsd.edu>
2024-05-02 10:36:49 -0700

    Add air horn and speach synthesizer

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,59 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+const synth = window.speechSynthesis;
+
 function init() {
-  // TODO
+
+  // Call populateVoiceList function
+  populateVoiceList();
+
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+
+  var btn = document.querySelector("button");
+  btn.addEventListener("click", readText);
+
+}
+
+// Add voices to select list
+function populateVoiceList() {
+  var sel = document.getElementById("voice-select");
+  let voices = synth.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    sel.appendChild(option);
+  }
+}
+
+// Read inputted text aloud
+function readText(){
+  let voices = synth.getVoices();
+  var input = document.getElementById("text-to-speak");
+  const speach = new SpeechSynthesisUtterance(input.value);
+
+  var sel = document.getElementById("voice-select");
+  const selectedOption = sel.selectedOptions[0].getAttribute("data-name");
+  for (let i = 0; i < voices.length; i++) {
+    if (voices[i].name === selectedOption) {
+      speach.voice = voices[i];
+    }
+  }
+
+  var speak_img = document.querySelector("img");
+
+  speach.addEventListener('start', function() {
+    speak_img.src = "assets/images/smiling-open.png";
+  });
+
+  speach.addEventListener('end', function() {
+    speak_img.src = "assets/images/smiling.png";
+
+  });
+
+  synth.speak(speach);
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,89 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // Drop-drop input
+  var drop_elem = document.getElementById("horn-select");
+  drop_elem.addEventListener("change", hornDropDown);
+
+  // Slider
+  var vol_elem = document.getElementById("volume");
+  vol_elem.addEventListener("input", volumeSlider);
+
+  // Button
+  var btn_elem = document.querySelector("#expose button");
+  btn_elem.addEventListener("click", playSound);
+
+
 }
+
+function hornDropDown(){
+  // Chosen horn
+  var horn_val = document.getElementById("horn-select").value;
+
+  var aud = document.getElementsByClassName("hidden");
+  var horn_img = document.querySelector("#expose img");
+
+  // If air horn
+  if (horn_val === "air-horn") {
+    horn_img.src = "assets/images/air-horn.svg";
+    aud.src = "assets/audio/air-horn.mp3";
+  } 
+  // If car horn
+  else if (horn_val === "car-horn") {
+    horn_img.src = "assets/images/car-horn.svg";
+    aud.src = "assets/audio/car-horn.mp3";
+  } 
+  // If party horn
+  else if (horn_val === "party-horn") {
+    horn_img.src = "assets/images/party-horn.svg";
+    aud.src = "assets/audio/party-horn.mp3";
+  }
+}
+
+function volumeSlider(){
+  // Slider volume 
+  var vol_val = document.getElementById("volume").value;
+
+  var aud = document.getElementsByClassName("hidden");
+  var vol_img = document.querySelector("#volume-controls img");
+
+  // Volume changes with slider
+  aud.volume = vol_val / 100;
+  
+  // If volume is 0
+  if (vol_val == 0){
+    vol_img.src = "assets/icons/volume-level-0.svg";
+  }
+  // If volume is 1 to 33
+  else if (vol_val > 0 && vol_val < 34){
+    vol_img.src = "assets/icons/volume-level-1.svg";
+  }
+  // If volume is 34 to 66
+  else if (vol_val > 33 && vol_val < 67){
+    vol_img.src = "assets/icons/volume-level-2.svg";
+  }
+  // If volume is 67+
+  else if (vol_val > 66){
+    vol_img.src = "assets/icons/volume-level-3.svg";
+  }
+}
+
+  function playSound(){
+    var aud = document.getElementsByClassName("hidden").src;
+    var horn_type = document.getElementById("horn-select").value;
+    var vol_val = document.getElementById("volume").value;
+
+
+    let full_aud = new Audio(aud);
+
+    // Volume changes updates with slider
+    full_aud.volume = vol_val / 100;
+    full_aud.play();
+
+    if (horn_type == "party-horn"){
+      const jsConfetti = new JSConfetti();
+      jsConfetti.addConfetti();
+    }
+
+}
+
