5cc01d3f112cac27614801f1c98b360f57f608cc
Dhaivat <dhaivatpachchigar@gmail.com>
2024-05-03 10:58:07 -0700

    need to fix path so it can display image

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -10,7 +10,7 @@ horn_select.addEventListener("change", () => {
   let horn_val = horn_select.value;
  
   if(horn_val === "air-horn"){
-    imag_select.src = 'assets\images\download.png';//bug: this file does not exists in the webbroswer
+    imag_select.src = 'assets\images\air-horn.svg';
     imag_select.alt = "air-horn selected";
     horn_audio.src = "assets\audio\air-horn.mp3";
   }
