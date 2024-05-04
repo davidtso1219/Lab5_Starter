732be3e989b3e93a9aad73ef258f6aea5dd41448
Andrew Pegg <andrewcomputsci2019@gmail.com>
2024-05-03 22:54:22 -0700

    finished unit testing section

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lab 5 - Starter
+
+
+## Part 3
+### Check your understanding
+- 1) No, making a unit test for this would not make sense, this is more of a integration test, verify that a process/component can interact with another process or feature. Unit testing if a user could write to another in a messaging system would not really be a unit test as it scope quickly relies on other parts of the code base outside the unit scope, as noted above a integration/system test would make more sense as we want to see the interplay of multiple components of the messaging system and the necessary other systems like a relay server etc.
+- 2) Yes using a unit test to verify the max message length feature does make sense as the scope of the test can be accomplished within the single part of the code base handling the message length checking, and this test would not rely on other features within the code base, making this a perfect candidate for unit testing.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,95 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+// phone Number Tests
+
+test('isPhoneNumberCorrectInput1', ()=>{
+  expect(isPhoneNumber("858-768-2124")).toBe(true);
+})
+
+test('isPhoneNumberCorrectInput2', ()=>{
+  expect(isPhoneNumber("967-668-2324")).toBe(true);
+})
+
+test('isPhoneNumberInCorrectInput1', ()=>{
+  expect(isPhoneNumber("9676682324")).toBe(false);
+})
+
+test('isPhoneNumberInCorrectInput2', ()=>{
+  expect(isPhoneNumber("374245455400126")).toBe(false);
+})
+
+// test isEmail function
+
+test('isEmailValidInput1', () =>{
+  expect(isEmail("isEmail@email.com")).toBe(true);
+})
+
+test('isEmailValidInput2', () =>{
+  expect(isEmail("noThankYou@yeet.io")).toBe(true);
+})
+
+test('isEmailInValidInput1', () =>{
+  expect(isEmail("noThankYouyeet.io")).toBe(false);
+})
+
+test('isEmailInValidInput2', () =>{
+  expect(isEmail("")).toBe(false);
+})
+
+// test isStrongPassword 
+
+test('isStrongPasswordValidInput1', ()=>{
+  expect(isStrongPassword('aStrongPassword')).toBe(true);
+})
+
+test('isStrongPasswordValidInput2', ()=>{
+  expect(isStrongPassword('securePassword_')).toBe(true);
+})
+
+test('isStrongPasswordInValidInput1', ()=>{
+  expect(isStrongPassword('securePassword_LongerThan15')).toBe(false);
+})
+
+test('isStrongPasswordInValidInput2', ()=>{
+  expect(isStrongPassword('cat')).toBe(false);
+})
+
+// test isDate function
+
+test('isDateValidInput1', ()=>{
+  expect(isDate('10/12/2024')).toBe(true);
+})
+
+test('isDateValidInput2', ()=>{
+  expect(isDate('6/9/2024')).toBe(true);
+})
+
+test('isDateInvalidInput1', ()=>{
+  expect(isDate('123/01/2001')).toBe(false);
+})
+
+test('isDateInvalidInput2', ()=>{
+  expect(isDate('09/08/21')).toBe(false);
+})
+
+
+// test isHexColor
+
+test('isHexColorValidInput1', ()=>{
+  expect(isHexColor('#fff')).toBe(true);
+})
+
+test('isHexColorValidInput2', ()=>{
+  expect(isHexColor('#4128d2')).toBe(true);
+})
+
+test('isHexColorInValidInput1', ()=>{
+  expect(isHexColor('##4128d2')).toBe(false);
+})
+
+test('isHexColorInValidInput2', ()=>{
+  expect(isHexColor('#fffffff')).toBe(false);
+})
+
