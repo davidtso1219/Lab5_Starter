44fcc9b39b08241f061e28d80a74e5c02368e2cc
DaPhysikist <therealpranavmehta@gmail.com>
2024-05-04 11:19:10 -0700

    finished part 1 of explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,51 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  let voiceSelect = document.getElementById("voice-select");
+  let speakButton = document.querySelector("button");
+  let textToSpeak = document.getElementById("text-to-speak");
+  let smileImg = document.querySelector("img");
+
+  function populateVoiceList() {
+    let voices = speechSynthesis.getVoices();
+  
+    voices.forEach(voice => {
+      const option = document.createElement("option");
+      option.textContent = `${voice.name} (${voice.lang})`;
+  
+      option.setAttribute("value", voice.name); 
+      option.setAttribute("data-lang", voice.lang);
+      option.setAttribute("data-name", voice.name);
+  
+      voiceSelect.appendChild(option);
+    });
+  }
+  
+  speechSynthesis.addEventListener("voiceschanged", populateVoiceList);
+
+  speakButton.addEventListener('click', function(){
+    smileImg.src = "assets/images/smiling-open.png";
+
+    let message = textToSpeak.value;
+    let utterance = new SpeechSynthesisUtterance(message);
+    let selectedVoice = voiceSelect.value;
+
+    let voices = speechSynthesis.getVoices();
+    voices.forEach(voice => {
+      if (voice.name === selectedVoice) {
+        utterance.voice = voice;
+      }
+    });
+
+    utterance.onstart = function(event) {
+      smileImg.src = "assets/images/smiling-open.png";
+    };
+
+    utterance.onend = function(event) {
+      smileImg.src = "assets/images/smiling.png";
+    };
+
+    speechSynthesis.speak(utterance);
+    utterance = null;
+  });
 }
