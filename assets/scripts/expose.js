2bc27cf393624a45ce23e7daba9c5347ee76c41a
mapersiani <122496033+mapersiani@users.noreply.github.com>
2024-05-05 16:09:08 -0700

    update image and audio on selection

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,13 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const selectElement = document.querySelector("select");
+  const image = document.querySelector("img");
+  const audio = document.querySelector("audio");
+
+  selectElement.addEventListener("change", (event) => {
+    image.src = `assets/images/${event.target.value}.svg`;
+    audio.src = `assets/audio/${event.target.value}.mp3`;
+  });
+
 }
