f01d8180f44c0713ed21f68c74c1a0da5eb8c82b
JosephToscano1 <toscano.joey13@gmail.com>
2024-05-05 16:47:36 -0700

    display image and set audio after horn select

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,29 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  let hornSelect = document.getElementById("horn-select");
+  hornSelect.addEventListener("change", displayHorn);
+
+  function displayHorn(horn){
+    const currHorn = horn.target.value;
+    let currImg = document.querySelector('img');
+    if(currHorn == "air-horn")
+    {
+      currImg.src = "assets/images/air-horn.svg";
+      let currAudio = document.querySelector('.hidden');
+      currAudio.src= "assets/audio/air-horn.mp3";
+    }
+    if(currHorn == "car-horn")
+    {
+      currImg.src = "assets/images/car-horn.svg";
+      let currAudio = document.querySelector('.hidden');
+      currAudio.src= "assets/audio/car-horn.mp3";
+    }
+    if(currHorn == "party-horn")
+    {
+      currImg.src = "assets/images/party-horn.svg";
+      let currAudio = document.querySelector('.hidden');
+      currAudio.src= "assets/audio/party-horn.mp3";
+    }
+  }
 }
