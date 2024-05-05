031ed8eaf4d02a4de653c1fec2b64b5de0aa719e
Timothy Herchen <timothy.herchen@gmail.com>
2024-05-05 12:32:04 -0700

    finish explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,47 @@
 // explore.js
 
-window.addEventListener('DOMContentLoaded', init);
+speechSynthesis.addEventListener('voiceschanged', init);
+
+let utteranceId = 0;
+let smileyElem;
+let ttsElem;
+
+function closeMouth(id) {
+    if (id === utteranceId) {
+        smileyElem.src = "assets/images/smiling.png";
+    }
+}
+
+function openMouth() {
+    smileyElem.src = "assets/images/smiling-open.png";
+}
 
 function init() {
-  // TODO
+    smileyElem = document.querySelector("#explore > img");
+    ttsElem = document.getElementById("text-to-speak");
+
+    ((select) => {
+        const children = speechSynthesis.getVoices()
+            .filter(({ lang }) => lang.startsWith("en"))
+            .map(({ name, lang }) => {
+                const option = document.createElement("option");
+
+                option.setAttribute("value", name);
+                option.setAttribute("alt", name);
+                option.innerText = `${name} (${lang})`;
+
+                return option;
+            });
+        select.replaceChildren(...children);
+
+        document.querySelector("#explore > button").addEventListener("click", (event) => {
+            const utterance = new SpeechSynthesisUtterance(ttsElem.value);
+            utterance.voice = speechSynthesis.getVoices()
+                .filter(({ name }) => name === select.value)[0];
+
+            openMouth();
+            speechSynthesis.speak(utterance);
+            utterance.addEventListener("end", closeMouth.bind(this, ++utteranceId));
+        });
+    })(document.getElementById("voice-select"));
 }
