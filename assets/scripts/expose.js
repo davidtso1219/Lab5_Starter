1019c1001b8deb19c80d4f1cb4019350e550a3a2
NathanD0wd <122576040+NathanD0wd@users.noreply.github.com>
2024-05-05 19:32:01 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,11 +1,15 @@
-/ expose.js
+// expose.js
+
 window.addEventListener(‘DOMContentLoaded’, init);
+
 function init() {
   let hornSelector = document.getElementById(‘horn-select’);
   let hornImage = document.querySelector(‘img’);
   let sound = document.querySelector(‘audio’);
+  
   hornSelector.addEventListener(‘change’, function () {
     let hornType = hornSelector.value;
+    
     switch (hornType) {
       case “air-horn”:
         hornImage.src = “assets/images/air-horn.svg”;
@@ -25,28 +29,35 @@ function init() {
         break;
     }
   })
+  
   let volumeSlider = document.getElementById(‘volume’);
   let volumeImage = document.querySelector(‘#volume-controls img’);
+  
   volumeSlider.addEventListener(‘input’, function () {
     let volumeValue = volumeSlider.value;
     sound.volume = volumeValue/100;
+    
     if (volumeValue == 0) {
       volumeImage.src = “assets/icons/volume-level-0.svg”;
       volumeImage.alt = “Volume level 0”;
     }
+      
     else if (volumeValue > 0 && volumeValue < 33) {
       volumeImage.src = “assets/icons/volume-level-1.svg”;
       volumeImage.alt = “Volume level 1”;
     }
+      
     else if (volumeValue >= 33 && volumeValue <= 66) {
       volumeImage.src = “assets/icons/volume-level-2.svg”;
       volumeImage.alt = “Volume level 2”;
     }
+      
     else {
       volumeImage.src = “assets/icons/volume-level-3.svg”;
       volumeImage.alt = “Volume level 3”;
     }
   })
+  
   let play = document.querySelector(‘button’);
   play.addEventListener(‘click’, function () {
     sound.play();
