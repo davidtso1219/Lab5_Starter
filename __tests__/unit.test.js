67fb5d8e134da80033d93a219f97c747be03f3e2
SatvikS7 <satviksriram7@gmail.com>
2024-05-05 20:35:20 -0700

    finished tests

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,6 +1,6 @@
 // unit.test.js
 
-//import test from 'node:test';
+import test from 'node:test';
 import {
   isPhoneNumber,
   isEmail,
@@ -12,36 +12,94 @@ import {
 
 //isPhoneNumber Tests:
 test('Is 5 a number?', () => {
-  expect(isPhoneNumber(/5/)).toBe(false);
+  expect(isPhoneNumber('5')).toBe(false);
 });
 
 test('Is 0000000000000000 a number?', () => {
-  expect(isPhoneNumber(/222222222222222/)).toBe(false);
+  expect(isPhoneNumber('222222222222222')).toBe(false);
 });
 
 test('Is 571-233-5259 a number?', () => {
-  expect(isPhoneNumber(/571-233-5259/)).toBe(true);
+  expect(isPhoneNumber('571-233-5259')).toBe(true);
 });
 
 test('Is 866-246-6453 a number?', () => {
-  expect(isPhoneNumber(/866-246-6453/)).toBe(true);
+  expect(isPhoneNumber('866-246-6453')).toBe(true);
 });
 
 
 
 //isEmail Tests: 
-test('Is a@a an email?', () => {
-  expect(isEmail(/a@a.c/)).toBe(false);
+test('Is a@a.c an email?', () => {
+  expect(isEmail('a@a.c')).toBe(false);
 });
 
-test('Is a@abcdedf an email?', () => {
-  expect(isEmail(/a@abcdedf.thomas/)).toBe(false);
+test('Is a@abcdedf.thomas an email?', () => {
+  expect(isEmail('a@abcdedf.thomas')).toBe(false);
 });
 
-test('Is s2sriram@ucsd.edu a number?', () => {
-  expect(isEmail(/sJss_xs9D@.Zu/)).toBe(true);
+test('Is s2sriram@ucsd.edu an email?', () => {
+  expect(isEmail('s2sriram@ucsd.edu')).toBe(true);
 });
 
-test('Is john@gmail.com a number?', () => {
-  expect(isEmail(/john@gmail.com/)).toBe(true);
+test('Is john@gmail.com an email?', () => {
+  expect(isEmail('john@gmail.com')).toBe(true);
 });
+
+
+
+//isStrongPassword Tests: 
+test('Is 9 a strong password?', () => {
+  expect(isStrongPassword('9')).toBe(false);
+});
+
+test('Is a#$^()adcUD an email?', () => {
+  expect(isStrongPassword('a#--adcUD')).toBe(false);
+});
+
+test('Is password a strong password?', () => {
+  expect(isStrongPassword('password')).toBe(true);
+});
+
+test('Is z7Cf1W a strong password?', () => {
+  expect(isStrongPassword('z7Cf1W')).toBe(true);
+});
+
+
+
+//isDate Tests: 
+test('Is /3/2000 a date?', () => {
+  expect(isDate('/3/2000')).toBe(false);
+});
+
+test('Is 2000 a date?', () => {
+  expect(isDate('2000')).toBe(false);
+});
+
+test('Is 99/99/9999 a date?', () => {
+  expect(isDate('99/99/9999')).toBe(true);
+});
+
+test('Is 5/5/2024 a date?', () => {
+  expect(isDate('5/5/2024')).toBe(true);
+});
+
+
+
+//isHexColor Tests: 
+test('Is red a date?', () => {
+  expect(isHexColor('red')).toBe(false);
+});
+
+test('Is 2000 a date?', () => {
+  expect(isHexColor('2000')).toBe(false);
+});
+
+test('Is #25d5Cb a date?', () => {
+  expect(isHexColor('#25d5Cb')).toBe(true);
+});
+
+test('Is 533dd3 a date?', () => {
+  expect(isHexColor('533dd3')).toBe(true);
+});
+
