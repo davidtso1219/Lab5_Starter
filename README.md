4a854ee0d2b31a7db77d83d345ba96d4c5cadc2f
SatvikS7 <satviksriram7@gmail.com>
2024-05-05 19:45:24 -0700

    began unit test

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab 5 - Starter
+1. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+- I would not use unit tests to test this "message" feature because this feature requires the interaction with another user meaning it needs to make calls to other applications. Therefore, using unit tests would not be applicible. 
+2. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+- Yes, I would use unit tests to test the "max message length" feature because 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,6 @@
 // unit.test.js
 
+//import test from 'node:test';
 import {
   isPhoneNumber,
   isEmail,
@@ -8,4 +9,39 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+
+//isPhoneNumber Tests:
+test('Is 5 a number?', () => {
+  expect(isPhoneNumber(/5/)).toBe(false);
+});
+
+test('Is 0000000000000000 a number?', () => {
+  expect(isPhoneNumber(/222222222222222/)).toBe(false);
+});
+
+test('Is 571-233-5259 a number?', () => {
+  expect(isPhoneNumber(/571-233-5259/)).toBe(true);
+});
+
+test('Is 866-246-6453 a number?', () => {
+  expect(isPhoneNumber(/866-246-6453/)).toBe(true);
+});
+
+
+
+//isEmail Tests: 
+test('Is a@a an email?', () => {
+  expect(isEmail(/a@a.c/)).toBe(false);
+});
+
+test('Is a@abcdedf an email?', () => {
+  expect(isEmail(/a@abcdedf.thomas/)).toBe(false);
+});
+
+test('Is s2sriram@ucsd.edu a number?', () => {
+  expect(isEmail(/sJss_xs9D@.Zu/)).toBe(true);
+});
+
+test('Is john@gmail.com a number?', () => {
+  expect(isEmail(/john@gmail.com/)).toBe(true);
+});
