2d4a2ad178b7639e7b0f77b606c38bde47e91832
David <yellow792508339@gmail.com>
2024-05-05 23:21:46 -0700

    explore and expose

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,14 +1,14 @@
 // explore.js
 
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   // TODO
   const synthesis = window.speechSynthesis;
-  const voices_select = document.getElementById('voice-select');
-  const input_text = document.getElementById('text-to-speak');
-  const btn = document.getElementById('btn_speak');
-  const pic = document.getElementById('smile');
+  const voices_select = document.getElementById("voice-select");
+  const input_text = document.getElementById("text-to-speak");
+  const btn = document.getElementsByTagName("button")[0];
+  const pic = document.getElementsByTagName("img")[0];
   var voices = []
 
   synthesis.onvoiceschanged = function() {
@@ -16,7 +16,7 @@ function init() {
 
     //fill out the language to the select box
     for(var i = 0; i < voices.length; i++) {
-      const option = document.createElement('option');
+      const option = document.createElement("option");
       option.textContent = `${voices[i].name} (${voices[i].lang})`;
   
       if (voices[i].default) {
@@ -28,22 +28,22 @@ function init() {
       voices_select.appendChild(option);
     }
 
-    const utter = new SpeechSynthesisUtterance('');
+    const utter = new SpeechSynthesisUtterance("");
 
     //open smile when start to speak
     utter.addEventListener("start", function() {
-      pic.setAttribute('src', './assets/images/smiling-open.png');
+      pic.setAttribute("src", "./assets/images/smiling-open.png");
     })
 
     //smile when end to speak
     utter.addEventListener("end", function() {
-      pic.setAttribute('src', './assets/images/smiling.png');
+      pic.setAttribute("src", "./assets/images/smiling.png");
     })
 
     //Speak the word when click the button
     btn.addEventListener("click", function() {
       utter.text = input_text.value;
-      const select_option = voices_select.selectedOptions[0].getAttribute("data-name");;
+      const select_option = voices_select.selectedOptions[0].getAttribute("data-name");
       for (let i = 0; i < voices.length; i++) {
         if (voices[i].name === select_option) {
           utter.voice = voices[i];
