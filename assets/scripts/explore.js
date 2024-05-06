4b12429b581f93d2cdec31da762a4757179b4b02
David <yellow792508339@gmail.com>
2024-05-05 19:43:02 -0700

    Complete Part_1

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,54 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const synthesis = window.speechSynthesis;
+  const voices_select = document.getElementById('voice-select');
+  const input_text = document.getElementById('text-to-speak');
+  const btn = document.getElementById('btn_speak');
+  const pic = document.getElementById('smile');
+  var voices = []
+
+  synthesis.onvoiceschanged = function() {
+    voices = synthesis.getVoices();
+
+    //fill out the language to the select box
+    for(var i = 0; i < voices.length; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+  
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voices_select.appendChild(option);
+    }
+
+    const utter = new SpeechSynthesisUtterance('');
+
+    //open smile when start to speak
+    utter.addEventListener("start", function() {
+      pic.setAttribute('src', './assets/images/smiling-open.png');
+    })
+
+    //smile when end to speak
+    utter.addEventListener("end", function() {
+      pic.setAttribute('src', './assets/images/smiling.png');
+    })
+
+    //Speak the word when click the button
+    btn.addEventListener("click", function() {
+      utter.text = input_text.value;
+      const select_option = voices_select.selectedOptions[0].getAttribute("data-name");;
+      for (let i = 0; i < voices.length; i++) {
+        if (voices[i].name === select_option) {
+          utter.voice = voices[i];
+        }
+      }
+      synthesis.speak(utter);
+    })
+  }
+
+
 }
