6eaf1c253d5cc0f5b0e1f7e56f5fa8c73b626d9b
Brandon Chung <BJHC051304@gmail.com>
2024-05-06 22:09:53 -0700

    testing branch: should be wrong

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lab 5 - Starter
+1. I would not write a unit test for the messages componenet. This is an individual feature you can test but there are other moving parts. While there are still isolated components that you are able to test the message feature is to wide to have a specific unit test for. 
+2. However if you are testing the max messsage length there is one specific test that you can run to look for the proper length. Unlike the entire messages component it is simply done with a check on the length. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,82 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+test('checks if "fda" is a strong password', () => {
+  expect(isStrongPassword("fda")).toBe(false);
+});
+
+test('checks if "fdafdafdafdafad123" is a strong password', () => {
+  expect(isStrongPassword("fdafdafdafdafad123")).toBe(false);
+});
+
+test('checks if "strongpassword" is a strong password', () => {
+  expect(isStrongPassword("strongpassword")).toBe(true);
+});
+
+test('checks if "superstrong123" is a strong password', () => {
+  expect(isStrongPassword("superstrong123")).toBe(true);
+});
+
+test('checks if "11/11/123" is a date', () => {
+  expect(isDate("11/11/123")).toBe(false);
+});
+
+test('checks if "1/11/123" is a date', () => {
+  expect(isDate("1/11/123")).toBe(false);
+});
+
+test('checks if "11/11/2004" is a date', () => {
+  expect(isDate("11/11/2004")).toBe(true);
+});
+
+test('checks if "11/11/2005" is a date', () => {
+  expect(isDate("11/11/2005")).toBe(true);
+});
+
+test('checks if "fda" is an email', () => {
+  expect(isEmail("fda")).toBe(false);
+});
+
+test('checks if "fda123" is an email', () => {
+  expect(isEmail("fda123")).toBe(false);
+});
+
+test('checks if "fda@gmail.com" is an email', () => {
+  expect(isEmail("fda@gmail.com")).toBe(true);
+});
+
+test('checks if "fda123@gmail.com" is an email', () => {
+  expect(isEmail("fda123@gmail.com")).toBe(true);
+});
+
+test('checks if "fda" is a phone number', () => {
+  expect(isPhoneNumber("fda")).toBe(false);
+});
+
+test('checks if "1243231234" is a phone number', () => {
+  expect(isPhoneNumber("1243231234")).toBe(false);
+});
+
+test('checks if "(661) 432-1234" is a phone number', () => {
+  expect(isPhoneNumber("(661) 432-1234")).toBe(true);
+});
+
+test('checks if "661-432-1234" is a phone number', () => {
+  expect(isPhoneNumber("661-432-1234")).toBe(true);
+});
+
+test('checks if "13" is a hex color', () => {
+  expect(isHexColor("13")).toBe(false);
+});
+
+test('checks if "1fds3" is a hex color', () => {
+  expect(isHexColor("1fds3")).toBe(false);
+});
+
+test('checks if "#112233" is a hex color', () => {
+  expect(isHexColor("#112233")).toBe(true);
+});
+
+test('checks if "#123" is a hex color', () => {
+  expect(isHexColor("#123")).toBe(true);
+});
