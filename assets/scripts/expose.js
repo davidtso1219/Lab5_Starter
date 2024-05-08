00133d4c47a5124c8cf13af4f007544cd2ed3b97
andrewtran5222 <andrewtran5222@gmail.com>
2024-05-08 01:34:38 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,45 +1,53 @@
 // expose.js
 
 window.addEventListener('DOMContentLoaded', init);
-
-// window.addEventListener('DOMContentLoaded', init);
 const button = document.querySelector('button');
-const select = document.querySelector('select');
-const textArea = document.querySelector('textarea');
-const face = document.querySelector('main img');
-const synth = window.speechSynthesis;
-synth.addEventListener('voiceschanged', init);
+const select = document.querySelector('#horn-select');
+const audio = document.querySelector('audio');
+const image = document.querySelector('img');
+const slider = document.querySelector('#volume');
+const icon = document.querySelector('div > img');
+const jsConfetti = new JSConfetti();
 
 function init() {
-  let voices = synth.getVoices();
-  const interval = setInterval(checkSpeaking, 500);
-  
-  for(let i = 0; i < voices.length; i++) {
-    const option = document.createElement('option');
-    option.text = voices[i].name;
-    option.setAttribute('value', voices[i].name);
-    select.appendChild(option);
-  }
-  
-  function checkSpeaking() {
-    if (synth.speaking) {
-      face.setAttribute('src', 'assets/images/smiling-open.png');
-    } else {
-      face.setAttribute('src', 'assets/images/smiling.png');
+  select.addEventListener('change', (e) => {
+    let value = select.value;
+    switch (value) {
+      case 'air-horn':
+        image.setAttribute('src', 'assets/images/air-horn.svg');
+        audio.setAttribute('src', 'assets/audio/air-horn.mp3');
+        break;
+      case 'car-horn':
+        image.setAttribute('src', 'assets/images/car-horn.svg');
+        audio.setAttribute('src', 'assets/audio/car-horn.mp3');
+        break;
+      case 'party-horn':
+        image.setAttribute('src', 'assets/images/party-horn.svg');
+        audio.setAttribute('src', 'assets/audio/party-horn.mp3');
+        break;
+      default:
+        image.setAttribute('src', 'assets/images/no-image.png');
     }
-  }
-  
-  button.addEventListener('click', (e) => { 
-    let utterance = new SpeechSynthesisUtterance(textArea.value);
-    let selectedOption = select.value;
-    for (let i = 0; i < voices.length; i++) {
-      if (voices[i].name === selectedOption) {
-        utterance.voice = voices[i];
-        utterance.lang = voices[i].lang;
-      }
-    }
+  });
 
-    synth.speak(utterance);
-    textArea.value = "";
-  })
+  button.addEventListener('click', (e) => {
+    audio.play();
+    if (select.value == 'party-horn') {
+      jsConfetti.addConfetti();
+    }
+  });
+
+  slider.addEventListener('change', (e) => {
+    let volume = slider.value;
+    audio.volume = volume / 100;
+    if (volume == 0) {
+      icon.setAttribute('src', 'assets/icons/volume-level-0.svg');
+    } else if (volume < 33) {
+      icon.setAttribute('src', 'assets/icons/volume-level-1.svg');
+    } else if (volume < 67) {
+      icon.setAttribute('src', 'assets/icons/volume-level-2.svg');
+    } else {
+      icon.setAttribute('src', 'assets/icons/volume-level-3.svg');
+    }
+  });
 }
