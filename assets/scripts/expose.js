68d065554eb8c4396b4a59ae290f4ef9e8d3fb95
andrewtran5222 <andrewtran5222@gmail.com>
2024-05-08 01:27:57 -0700

    Update explore.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,44 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+// window.addEventListener('DOMContentLoaded', init);
+const button = document.querySelector('button');
+const select = document.querySelector('select');
+const textArea = document.querySelector('textarea');
+const face = document.querySelector('main img');
+const synth = window.speechSynthesis;
+synth.addEventListener('voiceschanged', init);
+
 function init() {
-  // TODO
+  let voices = synth.getVoices();
+  const interval = setInterval(checkSpeaking, 500);
+  
+  for(let i = 0; i < voices.length; i++) {
+    const option = document.createElement('option');
+    option.text = voices[i].name;
+    option.setAttribute('value', voices[i].name);
+    select.appendChild(option);
+  }
+  
+  function checkSpeaking() {
+    if (synth.speaking) {
+      face.setAttribute('src', 'assets/images/smiling-open.png');
+    } else {
+      face.setAttribute('src', 'assets/images/smiling.png');
+    }
+  }
+  
+  button.addEventListener('click', (e) => { 
+    let utterance = new SpeechSynthesisUtterance(textArea.value);
+    let selectedOption = select.value;
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterance.voice = voices[i];
+        utterance.lang = voices[i].lang;
+      }
+    }
+
+    synth.speak(utterance);
+    textArea.value = "";
+  })
 }
