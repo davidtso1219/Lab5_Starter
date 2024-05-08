aec3e1999efd31d73795aa3b58b44d09959d23b0
David De Santiago <107174944+ddesantiag0@users.noreply.github.com>
2024-05-07 19:09:16 -0700

    Update sum.test.js

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -4,7 +4,7 @@ test('adds 1 + 2 to equal 3', () => {
   // TODO
   expect(sum(1 + 2)).toBe(3);
 });
-// Test adding zero
-test('adds 0 + 5 to equal 5', () => {
-  expect(sum(0, 5)).toBe(5);
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1,2)).toBe(3);
 });
