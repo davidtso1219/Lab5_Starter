c6c4a4349ab8e759c40fb074f3b90fb84f4db4b0
Anya Chernova <achernova@ucsd.edu>
2024-05-07 22:25:05 -0700

    added explore

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Lab 5 - Starter
 Anya Chernova
+
+[expose](https://ascher176.github.io/Lab5_Starter/expose.html)
+[explore](https://ascher176.github.io/Lab5_Starter/explore.html)
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,39 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  let voice = document.getElementById("voice-select");
+
+  function addVoices() {
+    const voices = speechSynthesis.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voice.appendChild(option);
+    }
+  }
+  addVoices();
+  speechSynthesis.onvoiceschanged = addVoices;
+
+  let button = document.querySelector("button");
+  let input = document.querySelector("textarea");
+  let smiley = document.querySelector("img");
+
+  button.addEventListener("click", function () {
+    const voices = speechSynthesis.getVoices();
+    const utter = new SpeechSynthesisUtterance(input.value);
+    const selectedOption = voice.selectedOptions[0].getAttribute("data-name");
+    for (let i = 0; i < voices.length; i++) {
+        if (voices[i].name === selectedOption) {
+            utter.voice = voices[i];
+        }
+    }
+    speechSynthesis.speak(utter);
+    smiley.src = "assets/images/smiling-open.png";
+
+    utter.onend = function () { smiley.src = "assets/images/smiling.png"; };
+  });
+
 }
