8ffd85c3ff4bb0a6bc5d25436b152c236de104fa
Jianing's Macbook <jianingsmacbook@JianingsdeMBP.san.rr.com>
2024-05-08 17:06:08 -0700

    fix the error

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,9 +2,9 @@
 
 test('adds 1 + 2 to equal 3', () => {
   // TODO
-  expect(1 + 2).toBe(4);
+  expect(1 + 2).toBe(3);
 });
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1,2)).toBe(4);
+    expect(sum(1,2)).toBe(3);
 });
